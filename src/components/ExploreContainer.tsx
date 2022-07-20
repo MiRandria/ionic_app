894688927b432e2946d632056a175fb19f68d097
MiRandria <hei.minosoa@gmail.com>
2022-07-20 22:15:34 +0300

    feat : add ionic app

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,146 @@
+import './ExploreContainer.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import React from 'react'
+
+export default function ExploreContainer() {
+  return (
+    <>
+    
+    <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>Collaborateur Comptable débutant</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>NUMEN MADAGASCAR</Card.Title>
+          <Card.Text>
+          • Prise en charge de la saisie des écritures ou l’indexation des documents comptables 
+          • Prise en charge de la tenue des dossiers : vérifie la saisie comptable, analyse les comptes, effectue les rapprochements…
+          </Card.Text>
+        </Card.Body>
+    </Card> <br />
+
+    <Card style={{ width: '18rem' }}>
+    <Card.Header>  <strong>TÉLÉ-SECRÉTAIRE FRANCOPHONE POLYVALENTE</strong>  </Card.Header>
+      <Card.Body>
+        <Card.Title>NOSYCOM</Card.Title>
+        <Card.Text>
+        description": "NOSYCOM (Antsahavola Antananarivo) recrute des : TELESECRETAIRES MEDICALES FRANCOPHONES POLYVALENTES (Femme) (appels entrants). 
+        Votre mission sera de gérer les appels, fixer les rendez-vous, noter les…
+      </Card.Text>
+      </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>Des Développeurs .NET confirmé/ sénior</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>BOCASAY</Card.Title>
+          <Card.Text>
+          Garant de la veille technologique, nous serons à l’écoute de vos propositions d’évolutions. Vous interviendrez sur le développement d'applications web autour des aspects suivants : _ Développement,…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>DEVELOPPEURS PYTHON</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>BOCASAY</Card.Title>
+          <Card.Text>
+          Au sein d’une équipe à taille humaine rassemblant des compétences variées, dans une bonne ambiance de travail, vous développerez en équipe des solutions web à fortes valeurs ajoutées. Garant de la veille…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>DEVELOPPEURS WORDPRESS ET PRESTASHOP -réf:DEV/0722</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>Les Ateliers du Capricorne</Card.Title>
+          <Card.Text>
+          • Développement et intégration site web sous Wordpress, Prestashop, web services et applicatifs. • Personnalisation de site web à partir de template. • Développement des modules sous Wordpress, Prestashop.…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>DEVELOPPEURS PHP -réf:DEV/07/22</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>Les Ateliers du Capricorne</Card.Title>
+          <Card.Text>
+          ‣ Développer des logiciels au sein de plateformes existantes. ‣ Respecter les bonnes pratiques de développement, build et delivery. ‣ Répondre aux besoins des clients au travers de nouvelles fonctionnalités.…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>DES STAGIAIRES CHARGE(E)S RH</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>Les Ateliers du Capricorne</Card.Title>
+          <Card.Text>
+          Avec un accompagnement par nos soins, le stagiaire aura pour mission d’apporter son appui sur trois volets: - L’administration du personnel : Gestion des dossiers du personnel, gestion des absences, gestions…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>CONSEILLERS CLIENTS</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>ODITY</Card.Title>
+          <Card.Text>
+          - Traitements d’appels : conseiller et assister la clientèle au téléphone - Traitements de mails - Traiter les demandes dans le respect de la qualité de la relation client - Garantir la satisfaction client
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>Assistant Projet Digitaux</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>Madajob RH</Card.Title>
+          <Card.Text>
+          - Traitement des projets en cours - Support pour l’envoi des sms en masse - Rédaction de divers articles - Établissement des rapports d’envoi des sms - Suggestion de stratégie d’envoi SMS - Gestion du…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>REDACTEUR WEB -réf:20072201</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>ROYAL COMMUNICATION</Card.Title>
+          <Card.Text>
+          Société en pleine expansion recherche un rédacteur web passionné par l'entrepreneuriat, le marketing et le développement personnel CDI - poste à plein temps 40h du lundi au vendredi Indemnité : 300…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>STAGIAIRES DEVELOPPEURS Web & Mobile JAVA</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>LIVENEXX</Card.Title>
+          <Card.Text>
+          Livenexx est une agence de développement web crée en 2016 qui compte désormais plus de trente-cinq collaborateurs dont le siège social est à l’île Maurice et le centre de production à Madagascar. Notre…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>Stagiaires Techniciens de Maintenance</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>Camusat Madagascar</Card.Title>
+          <Card.Text>
+          \"STAGIAIRES TECHNICIENS DE MAINTENANCE\" : FINALITÉS DU POSTE Sous la supervision du responsable d'agence le stagiaire technicien de maintenance est tenu de: Assister les techniciens de…
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>AIDE COMPTABLE - ANTSANITIA MAHAJUNGA</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>ANTSANITIA RESORT HOTEL RESTAURANT</Card.Title>
+          <Card.Text>
+          Suivi comptable \nfacturation \nclassement \nrapprochements bancaires
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+  <Card style={{ width: '18rem' }}>
+      <Card.Header>  <strong>Econome en hotellerie - ANTSANITIA - MAJUNGA</strong>  </Card.Header>
+        <Card.Body>
+          <Card.Title>ANTSANITIA RESORT HOTEL RESTAURANT</Card.Title>
+          <Card.Text>
+          Gestion de l'économat\nSuivi des achats (entrées et sorties de marchandises) \nNettoyage de l'économat \ncalcul des ratios \nManagement des magasiniers
+          </Card.Text>
+        </Card.Body>
+  </Card> <br />
+</>
+  );
+ 
+}
+
+
+
+
